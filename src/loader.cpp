4a9fac0c1442267cc61f9e90ed1fdf59f0fa717a
Greentea <vckuznetsov@gmail.com>
2025-06-15 16:52:17 +0300

    Фикс текстур

--- src/loader.cpp
+++ src/loader.cpp
@@ -73,8 +73,8 @@ bool load_texture(char const *filepath, GLuint *out_texture) {
 	stbi_image_free(data);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	*out_texture = tex;
 	return true;
